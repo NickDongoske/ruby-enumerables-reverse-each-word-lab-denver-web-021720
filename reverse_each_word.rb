eb4adffdb3b651996a2cdcabe48bdb08e70a39fd
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-13 02:51:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   # sentence1.split(" ").collect(&:reverse!).join(" ")
-  sentence1.split.collect {|s| s.reverse}.join("")
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  sentence1.split.collect {|s| s.reverse}.join(" ")
+  # sentence.split.collect {|word| word.reverse}.join(" ")
   
 end
